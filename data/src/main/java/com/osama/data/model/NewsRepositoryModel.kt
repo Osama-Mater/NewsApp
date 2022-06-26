6b43b28cc52498b09a83aa9c34b05a98b168e012
Osama Ahmed Mattar <osama01145@fci.zu.edu.eg>
2022-06-26 22:35:17 +0200

    - Add NewsRepositoryModel for :data module

---
+++ data/src/main/java/com/osama/data/model/NewsRepositoryModel.kt
@@ -0,0 +1,26 @@
+package com.osama.data.model
+
+
+import java.util.*
+
+data class NewsRepositoryModel(
+    val status: String,
+    val totalResults: Int,
+    val articles: List<Article>
+) {
+    data class Article(
+        val source: Source,
+        val author: String?,
+        val title: String,
+        val description: String?,
+        val url: String,
+        val urlToImage: String?,
+        val publishedAt: Date,
+        val content: String?
+    ) {
+        data class Source(
+            val id: String?,
+            val name: String
+        )
+    }
+}
