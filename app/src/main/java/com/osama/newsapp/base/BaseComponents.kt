7722aa17a0da849890407f4093f67c68fca9b979
Osama Ahmed Mattar <osama01145@fci.zu.edu.eg>
2022-06-28 01:08:59 +0200

    - apply some changes to BaseViewModel

--- app/src/main/java/com/osama/newsapp/base/BaseComponents.kt
+++ app/src/main/java/com/osama/newsapp/base/BaseComponents.kt
@@ -1,12 +1,11 @@
 package com.osama.newsapp.base
 
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.State
-import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.launch
 
@@ -25,8 +24,8 @@ abstract class BaseViewModel<
     private val initialState: UiState by lazy { setInitialState() }
     abstract fun setInitialState(): UiState
 
-    private val _viewState: MutableState<UiState> = mutableStateOf(initialState)
-    val viewState: State<UiState> = _viewState
+    private val _viewState: MutableStateFlow<UiState> = MutableStateFlow(initialState)
+    val viewState: StateFlow<UiState> = _viewState
 
     private val _event: MutableSharedFlow<Event> = MutableSharedFlow()
 
