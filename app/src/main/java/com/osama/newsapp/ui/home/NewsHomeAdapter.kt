a9ed6a6ed5cda785527df1a795e72b953e32ff51
Osama Ahmed Mattar <osama01145@fci.zu.edu.eg>
2022-07-03 17:27:14 +0200

    - NewsHomeAdapter

---
+++ app/src/main/java/com/osama/newsapp/ui/home/NewsHomeAdapter.kt
@@ -0,0 +1,57 @@
+package com.osama.newsapp.ui.home
+
+import android.text.format.DateUtils
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import com.osama.domain.model.NewsDomainModel
+import com.osama.newsapp.R
+import com.osama.newsapp.databinding.NewsItemCardViewBinding
+import java.util.*
+
+internal class NewsHomeAdapter : RecyclerView.Adapter<NewsHomeAdapter.NewsViewHolder>() {
+    private var _articles: List<NewsDomainModel.Article> = emptyList()
+
+    fun setData(articles: List<NewsDomainModel.Article>) {
+        _articles = articles
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsViewHolder {
+        return NewsViewHolder(
+            NewsItemCardViewBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: NewsViewHolder, position: Int) {
+        holder.bind(_articles[position])
+    }
+
+    override fun getItemCount(): Int = _articles.size
+
+    internal inner class NewsViewHolder(private val newsItemCardViewBinding: NewsItemCardViewBinding) :
+        RecyclerView.ViewHolder(newsItemCardViewBinding.root) {
+        fun bind(article: NewsDomainModel.Article) {
+            with(newsItemCardViewBinding) {
+                articleImage.load(article.urlToImage) {
+                    crossfade(true)
+                    error(R.drawable.ic_image)
+                    fallback(R.drawable.ic_image)
+                }
+                articleTitle.text = article.title
+                publishedTime.text = DateUtils.getRelativeTimeSpanString(
+                    article.publishedAt.time,
+                    Calendar.getInstance().timeInMillis,
+                    DateUtils.DAY_IN_MILLIS
+                )
+                articleSource.text = article.source.name
+            }
+        }
+    }
+
+}
