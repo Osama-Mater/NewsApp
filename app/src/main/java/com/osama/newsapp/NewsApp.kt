bfe7ec07872e9de322698ece42cc23b2a162bef4
Osama Ahmed Mattar <osama01145@fci.zu.edu.eg>
2022-07-03 17:26:53 +0200

    - apply DynamicColors from material3 to the app

--- app/src/main/java/com/osama/newsapp/NewsApp.kt
+++ app/src/main/java/com/osama/newsapp/NewsApp.kt
@@ -1,7 +1,13 @@
 package com.osama.newsapp
 
 import android.app.Application
+import com.google.android.material.color.DynamicColors
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class NewsApp : Application()
+class NewsApp : Application(){
+    override fun onCreate() {
+        super.onCreate()
+        DynamicColors.applyToActivitiesIfAvailable(this)
+    }
+}
