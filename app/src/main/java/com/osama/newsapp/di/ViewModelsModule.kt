484721800d0dd630f5515d457889809be6a70e4f
Osama Ahmed Mattar <osama01145@fci.zu.edu.eg>
2022-07-03 17:28:27 +0200

    - ViewModelsModule

---
+++ app/src/main/java/com/osama/newsapp/di/ViewModelsModule.kt
@@ -0,0 +1,26 @@
+package com.osama.newsapp.di
+
+import com.osama.domain.usecase.GetNews
+import com.osama.newsapp.ui.dashboard.DashboardViewModel
+import com.osama.newsapp.ui.home.HomeViewModel
+import com.osama.newsapp.ui.notifications.NotificationsViewModel
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ActivityRetainedComponent
+
+@Module
+@InstallIn(ActivityRetainedComponent::class)
+class ViewModelsModule {
+
+    @Provides
+    fun provideHomeViewModel(
+        getNews: GetNews,
+    ) = HomeViewModel(getNews)
+
+    @Provides
+    fun provideDashboardViewModel() = DashboardViewModel()
+
+    @Provides
+    fun provideNotificationsViewModel() = NotificationsViewModel()
+}
