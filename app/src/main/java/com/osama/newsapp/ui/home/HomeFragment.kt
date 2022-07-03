1e28e13ee5af7e9ee0105d84b0991c9fece08770
Osama Ahmed Mattar <osama01145@fci.zu.edu.eg>
2022-07-03 17:28:00 +0200

    - HomeFragment

--- app/src/main/java/com/osama/newsapp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/osama/newsapp/ui/home/HomeFragment.kt
@@ -4,35 +4,48 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.osama.newsapp.databinding.FragmentHomeBinding
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+
+    private val homeViewModel: HomeViewModel by viewModels()
+    private val newsHomeAdapter: NewsHomeAdapter = NewsHomeAdapter()
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
-
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+        return binding.root
+    }
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.recyclerView.apply {
+            setHasFixedSize(true)
+            adapter = newsHomeAdapter
         }
-        return root
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                homeViewModel.viewState.collect { homeState ->
+                    if (homeState.newsList.articles.isNotEmpty()) {
+                        newsHomeAdapter.setData(homeState.newsList.articles)
+                    }
+                }
+            }
+        }
     }
 
     override fun onDestroyView() {
