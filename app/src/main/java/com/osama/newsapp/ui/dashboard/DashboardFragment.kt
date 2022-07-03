5e1620aa7ad441f8d0c231c0d9c7d07e0bda860b
Osama Ahmed Mattar <osama01145@fci.zu.edu.eg>
2022-07-03 17:28:53 +0200

    - Dashboard

--- app/src/main/java/com/osama/newsapp/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/osama/newsapp/ui/dashboard/DashboardFragment.kt
@@ -6,9 +6,11 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.fragment.app.viewModels
 import com.osama.newsapp.databinding.FragmentDashboardBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class DashboardFragment : Fragment() {
 
     private var _binding: FragmentDashboardBinding? = null
@@ -22,8 +24,7 @@ class DashboardFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val dashboardViewModel =
-            ViewModelProvider(this).get(DashboardViewModel::class.java)
+        val dashboardViewModel: DashboardViewModel by viewModels()
 
         _binding = FragmentDashboardBinding.inflate(inflater, container, false)
         val root: View = binding.root
