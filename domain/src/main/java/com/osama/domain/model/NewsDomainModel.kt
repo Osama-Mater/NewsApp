4e70e8dd586deb6fcebb456fa7948a5b6784f631
Osama Ahmed Mattar <osama01145@fci.zu.edu.eg>
2022-07-04 00:49:42 +0200

    - NewsDomainModel implements Parcelable

--- domain/src/main/java/com/osama/domain/model/NewsDomainModel.kt
+++ domain/src/main/java/com/osama/domain/model/NewsDomainModel.kt
@@ -1,13 +1,17 @@
 package com.osama.domain.model
 
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 import java.util.*
 
+@Parcelize
 data class NewsDomainModel(
     val status: String,
     val totalResults: Int,
     val articles: List<Article>
-) {
+) : Parcelable {
+    @Parcelize
     data class Article(
         val source: Source,
         val author: String?,
@@ -17,10 +21,11 @@ data class NewsDomainModel(
         val urlToImage: String?,
         val publishedAt: Date,
         val content: String?
-    ) {
+    ) : Parcelable {
+        @Parcelize
         data class Source(
             val id: String?,
             val name: String
-        )
+        ) : Parcelable
     }
 }
