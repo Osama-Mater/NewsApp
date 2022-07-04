18c7d7436fce9151570de40ef012a91c8d4e345c
Osama Ahmed Mattar <osama01145@fci.zu.edu.eg>
2022-07-04 04:26:00 +0200

    ArticleDetailsFragment.kt

--- app/src/main/java/com/osama/newsapp/ui/home/ArticleDetailsFragment.kt
+++ app/src/main/java/com/osama/newsapp/ui/home/ArticleDetailsFragment.kt
@@ -1,20 +1,33 @@
 package com.osama.newsapp.ui.home
 
 import android.os.Bundle
+import android.text.Html
+import android.text.format.DateUtils
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.setFragmentResultListener
+import coil.load
 import com.osama.domain.model.NewsDomainModel
+import com.osama.newsapp.R
 import com.osama.newsapp.databinding.FragmentArticleDetailsBinding
 import dagger.hilt.android.AndroidEntryPoint
+import java.util.*
 
 @AndroidEntryPoint
 class ArticleDetailsFragment : Fragment() {
 
     private var _binding: FragmentArticleDetailsBinding? = null
     private val binding get() = _binding!!
+    private lateinit var article: NewsDomainModel.Article
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setFragmentResultListener(ARTICLE_DETAILS_BUNDLE) { requestKey, bundle ->
+            article = bundle.getParcelable(ARTICLE)!!
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,14 +35,32 @@ class ArticleDetailsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentArticleDetailsBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-        return root
+        binding.materialToolbar.setNavigationOnClickListener {
+            this@ArticleDetailsFragment.requireActivity().onBackPressed()
+        }
+
+        return binding.root
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        setFragmentResultListener(ARTICLE_DETAILS_BUNDLE) { requestKey, bundle ->
-            val result: NewsDomainModel.Article? = bundle.getParcelable(ARTICLE)
+    override fun onResume() {
+        super.onResume()
+        with(binding) {
+            articleImage.load(article.urlToImage) {
+                crossfade(true)
+                error(R.drawable.ic_image)
+                fallback(R.drawable.ic_image)
+            }
+            articleTitle.text = article.title
+            articleAuthor.text = article.author
+            articleSource.text = article.source.name
+            articlePublishedTime.text = DateUtils.getRelativeTimeSpanString(
+                article.publishedAt.time,
+                Calendar.getInstance().timeInMillis,
+                DateUtils.DAY_IN_MILLIS
+            )
+            articleDescription.text =
+                Html.fromHtml(article.description, Html.FROM_HTML_MODE_COMPACT)
+            articleContent.text = Html.fromHtml(article.content, Html.FROM_HTML_MODE_COMPACT)
         }
     }
 
